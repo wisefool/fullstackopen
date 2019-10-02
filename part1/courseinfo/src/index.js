1397e64c75eb5364937d3e867314fd5785023bf3
Aaron G Marx <aaron@mightybytes.com>
2019-10-02 16:42:36 -0500

    1.5 course information, step5 complete

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -15,7 +15,6 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-    {console.log(props.parts)}
   return (
     <div>
       <Part title={props.parts[0].name} exercise_count={props.parts[0].exercises}/>
@@ -33,27 +32,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises:  10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises:  10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 
